8c6987683d425c97b1369250aee9b232565fa04a
Becca King <becca.king3232@gmail.com>
2020-07-19 16:38:09 -0400

    Desktop breakpoints, rough json to csv solution

---
+++ conversion.js
@@ -0,0 +1,21 @@
+const csvjson = require('csvjson');
+const fs = require('fs');
+const stream = require('stream');
+const JSONStream = require('jsonstream');
+const createReadStream = fs.createReadStream;
+const createWriteStream = fs.createWriteStream;
+let line = 0;
+const transformJsonToCSV = new stream.Transform({
+    transform: function transformer(chunk, encoding, callback) {
+        line++;
+        callback(false, csvjson.toCSV(chunk, {
+            headers: line > 1 ? 'none' : 'key'
+        }));
+    },
+    readableObjectMode: true,
+    writableObjectMode: true,
+});
+createReadStream('./content.json', 'utf-8')
+    .pipe(JSONStream.parse('*'))
+    .pipe(transformJsonToCSV)
+    .pipe(createWriteStream('./content.csv'));
